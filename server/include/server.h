7b297e7e78efa70420c3e8326b81b7a3f01fec66
Florian Grima <florian@gamercraft.com>
2023-05-25 16:44:04 +0200

    add help command for server

---
+++ server/include/server.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** server.h
+** File description:
+** server.h
+*/
+
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
--- server/src/main.c
+++ server/src/main.c
@@ -1,5 +1,26 @@
-#include <stdio.h>
+/*
+** EPITECH PROJECT, 2023
+** main.c
+** File description:
+** main
+*/
 
-int main() {
-    printf("Hello, world!\n");
+#include "../include/server.h"
+
+void print_help() {
+    printf("USAGE: ./zappy_server -p port -x width -y height -n name1 name2 ");
+    printf("... -c clientsNb -f freq\n");
+    printf("        port        is the port number\n");
+    printf("        width       is the width of the world\n");
+    printf("        height      is the height of the world\n");
+    printf("        nameX       is the name of the team X\n");
+    printf("        clientsNb   is the number of authorized clients per team");
+    printf("\n        freq        is the reciprocal of");
+    printf(" time unit for execution of actions\n");
+}
+
+int main(int ac, char **av) {
+    if (strcmp(av[1], "-help") == 0 || strcmp(av[1], "-h") == 0) {
+        print_help();
+    }
 }
