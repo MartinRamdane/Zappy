fcd0fbcd61a8ada70ff9e367324bd0ea9daab7ef
Morais <gabriel.de-souza-morais@epitech.eu>
2023-05-30 17:37:10 +0200

    [fix] Delete file Tile.cpp

---
+++ gui/src/display/Tile.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-MAR-4-1-zappy-martin.ramdane
+** File description:
+** Tile
+*/
+
+#include "Tile.hpp"
+
+Tile::Tile(/* args */)
+{
+}
+
+Tile::~Tile()
+{
+}
