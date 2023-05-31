c77b57e6ddfc4decb130ec55f81e20c0e6600f04
Adam <adam@admlbs.fr>
2023-05-31 15:25:07 +0200

    Added generation of player based on team when connected

--- server/src/team_handling.c
+++ server/src/team_handling.c
@@ -42,16 +42,17 @@ int get_available_slots_in_team(server_t *s_infos, char *team)
 
 int add_client_to_team(server_t *s_infos, char *team, client_t *cli)
 {
-
+    if (strcmp(team, "GRAPHIC") == 0) {
+        cli->team_name = strdup(team); generate_gui_player(cli, cli->socket);
+        return 1;
+    }
     if (cli->team_name != NULL) return 0;
     int available_slots = get_available_slots_in_team(s_infos, team);
     char *buff = malloc(sizeof(char) * 100);
     sprintf(buff, "%d\n", available_slots);
-    if (does_team_exists(s_infos, team) == 0)
-        return -1;
+    if (does_team_exists(s_infos, team) == 0) return -1;
     if (does_team_has_space(s_infos, team) == 0) {
-        send(cli->socket, buff, strlen(buff), 0);
-        return -1;
+        send(cli->socket, buff, strlen(buff), 0); return -1;
     }
     team_t *tmp = NULL;
     LIST_FOREACH(tmp, &s_infos->team_head, next) {
@@ -59,10 +60,10 @@ int add_client_to_team(server_t *s_infos, char *team, client_t *cli)
             tmp->clients++;
             cli->team_name = strdup(team);
             send(cli->socket, buff, strlen(buff), 0);
+            generate_player(cli, cli->socket, team);
             return 1;
         }
-    }
-    return 0;
+    } return 0;
 }
 
 void remove_client_from_team(client_t *cli, server_t *s_infos)
