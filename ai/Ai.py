85a86c22c44227d5da52b2a4aa85342646ee7897
Ramdane Martin <martin.ramdane@epitech.eu>
2023-05-31 14:38:14 +0200

    add joinGame
    Co-authored-by: Luluri <Luluri@users.noreply.github.com>

--- ai/Ai.py
+++ ai/Ai.py
@@ -1,9 +1,48 @@
+from MyClient import Client
+import sys
 
 class Ai:
-    def __init__(self, teamName):
+    def __init__(self, teamName, machine, port):
         self.teamName = teamName
         self.mapSize = (0, 0)
         self.avaibleSlots = 0
+        self.machine = machine
+        self.port = port
+        self.seachFood = False
+        self.path = []
+        self.level = 1
+        self.client = None
+
+    def joinGame(self):
+        self.client = Client(self.machine, self.port)
+        self.client.connect()
+        receive = self.client.receive_message()
+        if receive == "WELCOME\n":
+            self.client.send_message(self.teamName + "\n")
+        receive = self.client.receive_message()
+        arr = receive.split('\n')
+        self.setAvaibleSlots(int(arr[0]))
+        self.setMapSize(arr[1])
+
+    def mainloop(self):
+        lookInventoryFood = 0
+        message = "Look\n"
+        receive = ""
+        while True:
+            if self.path:
+                message = self.path.pop(0)
+            elif self.seachFood and not self.path:
+                message = "Look\n"
+            elif lookInventoryFood >= 3 and not self.path:
+                message = "Inventory\n"
+                lookInventoryFood = 0
+            print("Send: " + message)
+            self.client.send_message(message)
+            receive = self.client.receive_message()
+            self.parseCommands(message, receive)
+            if not self.path:
+                lookInventoryFood += 1
+            message = "Look\n"
 
     def setMapSize(self, arr):
         tmp = arr.split(' ')
@@ -11,3 +50,86 @@ class Ai:
 
     def setAvaibleSlots(self, slots):
         self.avaibleSlots = slots
+
+    def getInventory(self, response):
+        data_string = response.replace("[", "").replace("]", "").replace(",", "")
+        data_list = data_string.split()
+        data_dict = {}
+        for i in range(0, len(data_list), 2):
+            key = data_list[i]
+            value = int(data_list[i+1])
+            data_dict[key] = value
+        return data_dict
+
+    def getObjectsAround(self, response):
+        data_string = response.replace("[", "").replace("]", "")
+        data_list = data_string.split(",")
+        result_list = []
+        for item in data_list:
+            inner_list = [i.strip() for i in item.split()]
+            if inner_list:
+                result_list.append(inner_list)
+            else:
+                result_list.append([])
+        return result_list
+
+    def parseCommands(self, message, receive):
+        print("Receive: " + receive)
+        if message == "Inventory\n" and receive != "ko\n":
+            inventory = self.getInventory(receive)
+            if inventory["food"] < 5:
+                self.seachFood = True
+        elif message == "Look\n" and receive != "ko\n":
+            getobjects = self.getObjectsAround(receive)
+            if self.seachFood:
+                nearestFood = self.getNearestObject("food", getobjects)
+                if nearestFood is None:
+                    self.path = ["Forward\n"]
+                    return
+                self.path = self.getPathtoObject(nearestFood, "food")
+                self.seachFood = False
+        elif message == "dead\n":
+            sys.exit()
+
+    def getNearestObject(self, name, objects):
+        indexes = []
+        for i, sous_liste in enumerate(objects):
+            if name in sous_liste:
+                indexes.append(i)
+        if not indexes:
+            return None
+        return min(indexes)
+
+    def getNearestNumberInList(self, number, liste):
+        nearest = None
+        difference_minimale = float('inf')
+        for i in liste:
+            difference = abs(i - number)
+            if difference < difference_minimale:
+                nearest = i
+                difference_minimale = difference
+            elif difference == difference_minimale and i > nearest:
+                nearest = i
+        return nearest
+
+    def getPathtoObject(self, case, item):
+        centers = [0, 2, 6, 12, 20, 30, 42, 56]
+        path = []
+        if case in centers:
+            index = centers.index(case)
+            for i in range(index):
+                path.append("Forward\n")
+            path.append("Take " + item + "\n")
+            return path
+        nearestNumber = self.getNearestNumberInList(case, centers)
+        index = centers.index(nearestNumber)
+        for i in range(index):
+            path.append("Forward\n")
+        if case > nearestNumber:
+            path.append("Right\n")
+        else:
+            path.append("Left\n")
+        for i in range(abs(case - nearestNumber)):
+            path.append("Forward\n")
+        path.append("Take " + item + "\n")
+        return path
--- ai/main.py
+++ ai/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-from MyClient import Client
 from Ai import Ai
 
 
@@ -10,26 +9,6 @@ def print_help():
     print('\tport is the port number')
     print('\tname is the name of the team')
     print('\tmachine is the name of the machine; localhost by default')
-
-def mainloop(machine, port, name):
-    client = Client(machine, port)
-    client.connect()
-    ai = Ai(name)
-    message = client.receive_message()
-    if message == "WELCOME\n":
-        client.send_message(name + "\n")
-    message = client.receive_message()
-    arr = message.split('\n')
-    ai.setAvaibleSlots(int(arr[0]))
-    ai.setMapSize(arr[1])
-    print("Name: " + ai.teamName)
-    print("Slots: " + str(ai.avaibleSlots))
-    print("Map size: " + str(ai.mapSize))
-    while True:
-        tmp = input()
-        client.send_message(tmp + "\n")
-        message = client.receive_message()
-        print(message)
 
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == '-help':
@@ -44,7 +23,10 @@ def main():
     port = int(sys.argv[2])
     name = sys.argv[4]
     machine = sys.argv[6]
-    mainloop(machine, port, name)
+    ai = Ai(name, machine, port)
+    ai.joinGame()
+    ai.mainloop()
+
 
 if __name__ == '__main__':
     main()
