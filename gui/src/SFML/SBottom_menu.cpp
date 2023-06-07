e2035bb8bbf084944411dc884e3466d9aa42dd65
Morais <gabriel.de-souza-morais@epitech.eu>
2023-06-07 14:17:08 +0200

    [feature] Names of gems and food added to bottom menu

--- gui/src/SFML/SBottom_menu.cpp
+++ gui/src/SFML/SBottom_menu.cpp
@@ -116,75 +116,145 @@ void SBottom_menu::createTexts()
     this->_texts["food"] = std::make_shared<sf::Text>();
     this->_texts["food"]->setFont(this->_font);
     this->_texts["food"]->setString("0");
-    this->_texts["food"]->setCharacterSize(20);
+    this->_texts["food"]->setCharacterSize(40);
     this->_texts["food"]->setFillColor(sf::Color::White);
+
+    this->_texts["foodName"] = std::make_shared<sf::Text>();
+    this->_texts["foodName"]->setFont(this->_font);
+    this->_texts["foodName"]->setString("Food");
+    this->_texts["foodName"]->setCharacterSize(35);
+    this->_texts["foodName"]->setFillColor(sf::Color::White);
 
     this->_texts["linemate"] = std::make_shared<sf::Text>();
     this->_texts["linemate"]->setFont(this->_font);
     this->_texts["linemate"]->setString("0");
-    this->_texts["linemate"]->setCharacterSize(20);
+    this->_texts["linemate"]->setCharacterSize(40);
     this->_texts["linemate"]->setFillColor(sf::Color::White);
+
+    this->_texts["linemateName"] = std::make_shared<sf::Text>();
+    this->_texts["linemateName"]->setFont(this->_font);
+    this->_texts["linemateName"]->setString("Linemate");
+    this->_texts["linemateName"]->setCharacterSize(35);
+    this->_texts["linemateName"]->setFillColor(sf::Color::White);
 
     this->_texts["deraumere"] = std::make_shared<sf::Text>();
     this->_texts["deraumere"]->setFont(this->_font);
     this->_texts["deraumere"]->setString("0");
-    this->_texts["deraumere"]->setCharacterSize(20);
+    this->_texts["deraumere"]->setCharacterSize(40);
     this->_texts["deraumere"]->setFillColor(sf::Color::White);
+
+    this->_texts["deraumereName"] = std::make_shared<sf::Text>();
+    this->_texts["deraumereName"]->setFont(this->_font);
+    this->_texts["deraumereName"]->setString("Deraumere");
+    this->_texts["deraumereName"]->setCharacterSize(35);
+    this->_texts["deraumereName"]->setFillColor(sf::Color::White);
 
     this->_texts["sibur"] = std::make_shared<sf::Text>();
     this->_texts["sibur"]->setFont(this->_font);
     this->_texts["sibur"]->setString("0");
-    this->_texts["sibur"]->setCharacterSize(20);
+    this->_texts["sibur"]->setCharacterSize(40);
     this->_texts["sibur"]->setFillColor(sf::Color::White);
+
+    this->_texts["siburName"] = std::make_shared<sf::Text>();
+    this->_texts["siburName"]->setFont(this->_font);
+    this->_texts["siburName"]->setString("Sibur");
+    this->_texts["siburName"]->setCharacterSize(35);
+    this->_texts["siburName"]->setFillColor(sf::Color::White);
 
     this->_texts["mendiane"] = std::make_shared<sf::Text>();
     this->_texts["mendiane"]->setFont(this->_font);
     this->_texts["mendiane"]->setString("0");
-    this->_texts["mendiane"]->setCharacterSize(20);
+    this->_texts["mendiane"]->setCharacterSize(40);
     this->_texts["mendiane"]->setFillColor(sf::Color::White);
+
+    this->_texts["mendianeName"] = std::make_shared<sf::Text>();
+    this->_texts["mendianeName"]->setFont(this->_font);
+    this->_texts["mendianeName"]->setString("Mendiane");
+    this->_texts["mendianeName"]->setCharacterSize(35);
+    this->_texts["mendianeName"]->setFillColor(sf::Color::White);
 
     this->_texts["phiras"] = std::make_shared<sf::Text>();
     this->_texts["phiras"]->setFont(this->_font);
     this->_texts["phiras"]->setString("0");
-    this->_texts["phiras"]->setCharacterSize(20);
+    this->_texts["phiras"]->setCharacterSize(40);
     this->_texts["phiras"]->setFillColor(sf::Color::White);
+
+    this->_texts["phirasName"] = std::make_shared<sf::Text>();
+    this->_texts["phirasName"]->setFont(this->_font);
+    this->_texts["phirasName"]->setString("Phiras");
+    this->_texts["phirasName"]->setCharacterSize(35);
+    this->_texts["phirasName"]->setFillColor(sf::Color::White);
 
     this->_texts["thystame"] = std::make_shared<sf::Text>();
     this->_texts["thystame"]->setFont(this->_font);
     this->_texts["thystame"]->setString("0");
-    this->_texts["thystame"]->setCharacterSize(20);
+    this->_texts["thystame"]->setCharacterSize(40);
     this->_texts["thystame"]->setFillColor(sf::Color::White);
+
+    this->_texts["thystameName"] = std::make_shared<sf::Text>();
+    this->_texts["thystameName"]->setFont(this->_font);
+    this->_texts["thystameName"]->setString("Thystame");
+    this->_texts["thystameName"]->setCharacterSize(35);
+    this->_texts["thystameName"]->setFillColor(sf::Color::White);
 
     float distance = this->_sprites["bottom_menu"]->getGlobalBounds().width / this->_texts.size();
     float scale;
 
-    this->_texts["food"]->setPosition(sf::Vector2f(this->_sprites["food"]->getPosition().x, this->_sprites["food"]->getPosition().y + this->_sprites["food"]->getGlobalBounds().height / 2));
+    this->_texts["food"]->setPosition(sf::Vector2f(this->_sprites["food"]->getPosition().x - 10, this->_sprites["food"]->getPosition().y + this->_sprites["food"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["food"]->getGlobalBounds().width * 0.4;
     this->_texts["food"]->setScale(sf::Vector2f(scale, scale));
 
-    this->_texts["linemate"]->setPosition(sf::Vector2f(this->_sprites["linemate"]->getPosition().x, this->_sprites["linemate"]->getPosition().y + this->_sprites["linemate"]->getGlobalBounds().height / 2));
+    this->_texts["linemate"]->setPosition(sf::Vector2f(this->_sprites["linemate"]->getPosition().x - 8, this->_sprites["linemate"]->getPosition().y + this->_sprites["linemate"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["linemate"]->getGlobalBounds().width * 0.4;
     this->_texts["linemate"]->setScale(sf::Vector2f(scale, scale));
 
-    this->_texts["deraumere"]->setPosition(sf::Vector2f(this->_sprites["deraumere"]->getPosition().x, this->_sprites["deraumere"]->getPosition().y + this->_sprites["deraumere"]->getGlobalBounds().height / 2));
+    this->_texts["deraumere"]->setPosition(sf::Vector2f(this->_sprites["deraumere"]->getPosition().x - 8, this->_sprites["deraumere"]->getPosition().y + this->_sprites["deraumere"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["deraumere"]->getGlobalBounds().width * 0.4;
     this->_texts["deraumere"]->setScale(sf::Vector2f(scale, scale));
 
-    this->_texts["sibur"]->setPosition(sf::Vector2f(this->_sprites["sibur"]->getPosition().x, this->_sprites["sibur"]->getPosition().y + this->_sprites["sibur"]->getGlobalBounds().height / 2));
+    this->_texts["sibur"]->setPosition(sf::Vector2f(this->_sprites["sibur"]->getPosition().x - 8, this->_sprites["sibur"]->getPosition().y + this->_sprites["sibur"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["sibur"]->getGlobalBounds().width * 0.4;
     this->_texts["sibur"]->setScale(sf::Vector2f(scale, scale));
 
-    this->_texts["mendiane"]->setPosition(sf::Vector2f(this->_sprites["mendiane"]->getPosition().x, this->_sprites["mendiane"]->getPosition().y + this->_sprites["mendiane"]->getGlobalBounds().height / 2));
+    this->_texts["mendiane"]->setPosition(sf::Vector2f(this->_sprites["mendiane"]->getPosition().x - 8, this->_sprites["mendiane"]->getPosition().y + this->_sprites["mendiane"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["mendiane"]->getGlobalBounds().width * 0.4;
     this->_texts["mendiane"]->setScale(sf::Vector2f(scale, scale));
 
-    this->_texts["phiras"]->setPosition(sf::Vector2f(this->_sprites["phiras"]->getPosition().x, this->_sprites["phiras"]->getPosition().y + this->_sprites["phiras"]->getGlobalBounds().height / 2));
+    this->_texts["phiras"]->setPosition(sf::Vector2f(this->_sprites["phiras"]->getPosition().x - 8, this->_sprites["phiras"]->getPosition().y + this->_sprites["phiras"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["phiras"]->getGlobalBounds().width * 0.4;
     this->_texts["phiras"]->setScale(sf::Vector2f(scale, scale));
 
-    this->_texts["thystame"]->setPosition(sf::Vector2f(this->_sprites["thystame"]->getPosition().x, this->_sprites["thystame"]->getPosition().y + this->_sprites["thystame"]->getGlobalBounds().height / 2));
+    this->_texts["thystame"]->setPosition(sf::Vector2f(this->_sprites["thystame"]->getPosition().x - 8, this->_sprites["thystame"]->getPosition().y + this->_sprites["thystame"]->getGlobalBounds().height / 2));
     scale = distance / this->_sprites["thystame"]->getGlobalBounds().width * 0.4;
     this->_texts["thystame"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["foodName"]->setPosition(sf::Vector2f(this->_sprites["food"]->getPosition().x - 20, this->_sprites["food"]->getPosition().y - this->_sprites["food"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["food"]->getGlobalBounds().width * 0.4;
+    this->_texts["foodName"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["linemateName"]->setPosition(sf::Vector2f(this->_sprites["linemate"]->getPosition().x - 50, this->_sprites["linemate"]->getPosition().y - this->_sprites["linemate"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["linemate"]->getGlobalBounds().width * 0.4;
+    this->_texts["linemateName"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["deraumereName"]->setPosition(sf::Vector2f(this->_sprites["deraumere"]->getPosition().x - 50, this->_sprites["deraumere"]->getPosition().y - this->_sprites["deraumere"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["deraumere"]->getGlobalBounds().width * 0.4;
+    this->_texts["deraumereName"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["siburName"]->setPosition(sf::Vector2f(this->_sprites["sibur"]->getPosition().x - 25, this->_sprites["sibur"]->getPosition().y - this->_sprites["sibur"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["sibur"]->getGlobalBounds().width * 0.4;
+    this->_texts["siburName"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["mendianeName"]->setPosition(sf::Vector2f(this->_sprites["mendiane"]->getPosition().x - 45, this->_sprites["mendiane"]->getPosition().y - this->_sprites["mendiane"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["mendiane"]->getGlobalBounds().width * 0.4;
+    this->_texts["mendianeName"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["phirasName"]->setPosition(sf::Vector2f(this->_sprites["phiras"]->getPosition().x - 30, this->_sprites["phiras"]->getPosition().y - this->_sprites["phiras"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["phiras"]->getGlobalBounds().width * 0.4;
+    this->_texts["phirasName"]->setScale(sf::Vector2f(scale, scale));
+
+    this->_texts["thystameName"]->setPosition(sf::Vector2f(this->_sprites["thystame"]->getPosition().x - 50, this->_sprites["thystame"]->getPosition().y - this->_sprites["thystame"]->getGlobalBounds().height / 2 - 13));
+    scale = distance / this->_sprites["thystame"]->getGlobalBounds().width * 0.4;
+    this->_texts["thystameName"]->setScale(sf::Vector2f(scale, scale));
 }
 
 void SBottom_menu::draw(sf::RenderWindow &window)
