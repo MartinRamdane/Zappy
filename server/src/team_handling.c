2c6251b2a89365468bb9ddb603168e81a3116f69
Florian Grima <florian@gamercraft.com>
2023-06-16 10:01:05 +0200

    fix welcome message

--- server/src/team_handling.c
+++ server/src/team_handling.c
@@ -60,12 +60,12 @@ int add_client_to_team(server_t *s_infos, char *team, client_t *cli)
     LIST_FOREACH(tmp, &s_infos->team_head, next) {
         if (strcmp(tmp->name, team) == 0) {
             tmp->clients++; cli->team_name = strdup(team);
-            send(cli->socket, buff, strlen(buff), 0);
+            // send(cli->socket, buff, strlen(buff), 0);
             generate_player(s_infos, cli, cli->socket, team);
             char *buffer_life = strdup("LifeCycle");
             double task_food_time = calculate_time_for_task(s_infos, buffer_life);
             add_task(s_infos, buffer_life, task_food_time, cli);
-            sprintf(buff, "%d %d\n", s_infos->width, s_infos->height);
+            sprintf(buff, "%d\n%d %d\n", available_slots, s_infos->width, s_infos->height);
             send(cli->socket, buff, strlen(buff), 0);
             send_new_player_connected(s_infos, cli); return 1;
         }
