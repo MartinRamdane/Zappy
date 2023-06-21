a6c3bfc69427f2fd8edd6026e1b69616834c45f9
Morais <gabriel.de-souza-morais@epitech.eu>
2023-06-21 11:35:23 +0200

    [feature] Setup menu for florian

---
+++ gui/src/menu/Menu.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-MAR-4-1-zappy-martin.ramdane
+** File description:
+** Menu
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+
+class Menu {
+    public:
+        Menu();
+        ~Menu();
+
+    protected:
+    private:
+
+};
